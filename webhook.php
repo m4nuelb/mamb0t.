9dac57b4337aa3eb0bab335eaed4150721dbcca1
m4nuelb <bertozzi_manuel@alice.it>
2016-08-21 16:17:39 +0200

    test

--- webhook.php
+++ webhook.php
@@ -4,15 +4,44 @@ require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $bot->run();
 $messages = $bot->getReceivedMessages();
+
+$response1 = "comandi attivi: bau \r\n cibo";
+                             
+$bool = 0;
+
 foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
 	if($message->text)
 	{
-		$bot->sendTextMessage($recipientId, $message->text);
+      
+		$bot->sendTextMessage($recipientId, $response1);
+       
+     
+              $response = processRequest($message->text);
+              $bot->sendTextMessage($recipientId, $response);
+    
+        
+        
 	}
-	elseif($message->attachments)
+	
+}
+function processRequest($text)
+{
+	$text = trim($text);
+	$text = strtolower($text);
+	$response = "";
+	if($text=="bau")
 	{
-		$bot->sendTextMessage($recipientId, "Attachment received");
+		$response = "BAU BAU BAU BAU";
 	}
+	elseif ($text=="cibo")
+	{
+		$response = "http://www.arcaplanet.it/cane/cibo-per-cani/cibo-secco.html";
+	}
+	else
+	{
+		$response = "analfabeta non sai leggere, i comandi attivi sono due!!";
+	}
+	return $response;
 }
